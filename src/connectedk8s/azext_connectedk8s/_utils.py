579ae8ef7493fb8ca5a73134f90b9407a328f7ff
Wei Shi <weshi1@microsoft.com>
2023-03-13 19:18:45 -0700

    add comments

--- src/connectedk8s/azext_connectedk8s/_utils.py
+++ src/connectedk8s/azext_connectedk8s/_utils.py
@@ -309,15 +309,18 @@ def helm_install_release(chart_path, azure_arc_agent_version, subscription_id, k
                         "--set", "global.azureEnvironment={}".format(environment_name),
                         "--set", "systemDefaultValues.clusterconnect-agent.enabled=true",
                         "--output", "json"]
+    
+    # START: on-premise modification
+    # TODO: add check to see if current environment is on-premise.
     azure_arc_agent_version = "0.0.4"
     print(f"using azure_arc_agent_version version: {azure_arc_agent_version}")
-    # TODO: addd check to see if current environment is on-premise.
-    # START: on-premise modification
+    # TODO: remove hardcoded urls and use metadata_endpoints dictionary to get the values after endpoints are available.
     resource_manager = "https://resourcemanagerweb.azs:40007/"
     notification_endpoint = "http://notificationsapi.gnsdp.azs:4909/"
     config_endpoint = "https://configwebdp.configrp.azs:4914"
     his_endpoint = "https://his.devfabric.azs.microsoft.com"
     relay_endpoint = ".servicebus.azs.microsoft.com"
+
     cmd_helm_install.extend(
         [ 
             "--set", "systemDefaultValues.image.repository=arcaserver.azurecr.io",
